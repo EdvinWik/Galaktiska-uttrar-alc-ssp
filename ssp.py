d170956d38acc458f307736b203018f22cdd0c3b
EdvinWik <edvin.wik@elev.ga.ntig.se>
2022-03-28 12:57:47 +0200

    🎨

--- ssp.py
+++ ssp.py
@@ -1,4 +1,14 @@
+from random import random
+sten = 1
+sax = 2
+påse = 3
+gamer = ""
+dator = ""
+import random
+
 print("Välkommen till sten, sax eller påse.")
 svar = input("[1] Spela\n[2] Exit\n")
 while svar == "1":
-    print("spel")
+    gamer = input ("Välj mellan\n[1]Sten\n[2]Sax\n[3]Påse\n")
+    dator = str(random.randint (1,3))
+    
